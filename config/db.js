bb3bec20bf422cf583b88d76865a19e142e396e4
Henrique Souza <156008953+jshenriq@users.noreply.github.com>
2026-01-22 15:41:26 -0300

    update database connection to use connectionString for deploy

--- config/db.js
+++ config/db.js
@@ -4,11 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: String(process.env.DB_PASSWORD),
-  port: parseInt(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default db;
